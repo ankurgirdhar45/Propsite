daf8bd87a9600a774c8388842c17a9ee5a8118d2
ankur <ankur.girdhar@proptiger.com>
2015-03-17 02:20:58 +0530

    almost done

--- src/Progs/Performance.java
+++ src/Progs/Performance.java
@@ -2,7 +2,9 @@ package Progs;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
@@ -12,20 +14,49 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 
 
 public class Performance 
-{
-	/*
-	public void checkPerformance() throws InterruptedException
+{	
+	Object[][] ob;
+	FileInputStream fin;
+	HSSFWorkbook wb;
+	HSSFSheet sh;
+	HSSFCell cell;
+	HSSFRow row;
+	@DataProvider(name="ipURLs")
+	public Object[][] inputURLs()
+	{
+		
+		try
+		{
+		fin = new FileInputStream("performance.xls");
+		wb = new HSSFWorkbook(fin);
+		sh=wb.getSheetAt(0);
+		int lrow=sh.getLastRowNum();
+		ob = new Object[lrow+1][1];
+		for (int i=0;i<=lrow;i++)
+		{
+			row=sh.getRow(i);
+			cell=row.getCell(0);
+			ob[i][0]=cell.getStringCellValue();
+		}
+		}catch(IOException e){System.out.println("file kona mili");}
+		return ob;
+		
+		
+	}
+	
+	@Test(dataProvider="ipURLs")
+	public void checkPerformance(String url) throws InterruptedException
 	{
 		WebDriver d = new FirefoxDriver();
 		d.get("http://www.webpagetest.org");
 		WebElement URL=d.findElement(By.id("url"));
-		String urlc="www.proptiger.com";
-		URL.sendKeys(urlc);
+		URL.sendKeys(url);
 		WebElement startbutton = d.findElement(By.xpath("//input[@type='submit']"));
 		startbutton.click();
 		d.manage().timeouts().implicitlyWait(180, TimeUnit.SECONDS);
@@ -34,72 +65,51 @@ public class Performance
 			d.findElement(By.xpath("//input[@value='Re-run the test']"));
 		}catch(Exception E){System.out.println("kuch mila");}
 		System.out.println("loaded");
-		afterResult(d,urlc);
-	}*/
-	@Test
-	public void afterResult(/*WebDriver d,String url*/)
+		afterResult(d,url);
+	}
+	
+	public void afterResult(WebDriver d,String url)
 	{
-		WebDriver d = new FirefoxDriver();
-		String URL="https://www.proptiger.com";
-		d.get("http://www.webpagetest.org/result/150316_QE_GHE/");
-		
 		List<WebElement> fv= d.findElements(By.xpath("//table[@id='tableResults']/tbody/tr[3]/td"));
 		List<WebElement> rv= d.findElements(By.xpath("//table[@id='tableResults']/tbody/tr[4]/td"));
 		System.out.println(fv.size() + "    " + rv.size());
-		//String FVloadtime = d.findElement(By.id("fvLoadTime")).getText();
-		//String FVfirstbyte = d.findElement(By.id("fvTTFB")).getText();
-		
-		
-		//String RVloadtime = d.findElement(By.id("rvLoadTime")).getText();
-		
-		//System.out.println("Page Load time for first View " + FVTime + "\n" + "Repeat View Load Time " + RVTime);
-		
-		
 		try
 		{
+		/*	
 		FileInputStream filein = new FileInputStream("performance.xls");
-	    HSSFWorkbook workbookr = new HSSFWorkbook(filein);
-	    HSSFSheet sheetr = workbookr.getSheetAt(1);
-	    int last_row=sheetr.getLastRowNum();
-	    System.out.println("Last row is " + last_row);
-	    HSSFRow rown = sheetr.createRow(last_row+1);
-	    HSSFCell cellA1 = rown.createCell(0);
-	    cellA1.setCellValue(URL);
+	    HSSFWorkbook workbookr = new HSSFWorkbook(filein);*/
+	    //HSSFSheet sheetr = workbookr.getSheetAt(1);
+	    sh=wb.getSheetAt(1);
+		int lrow=sh.getLastRowNum();
+	    row = sh.createRow(lrow+1);
+	    cell = row.createCell(0);
+	    cell.setCellValue(url);
 	    int n = 1;
 	    for (WebElement e : fv)
 	    {
-	    	HSSFCell x = rown.createCell(n);
-	    	x.setCellValue(e.getText());
+	    	cell = row.createCell(n);
+	    	cell.setCellValue(e.getText());
 	    	n+=1;
 	    }
-	    HSSFCell cellA4 = rown.createCell(n);
-	    cellA4.setCellValue(d.getCurrentUrl());
-	    HSSFRow rown2 = sheetr.createRow(last_row+2);
-	    HSSFCell cellB1 = rown2.createCell(0);
-	    cellB1.setCellValue("");
+	    cell = row.createCell(n);
+	    cell.setCellValue(d.getCurrentUrl());
+	    row = sh.createRow(lrow+2);
+	    cell = row.createCell(0);
+	    cell.setCellValue("");
 	    n = 1;
 	    for (WebElement e : rv)
 	    {
-	    	HSSFCell x = rown2.createCell(n);
-	    	x.setCellValue(e.getText());
+	    	cell = row.createCell(n);
+	    	cell.setCellValue(e.getText());
 	    	n+=1;
 	    }
-	    HSSFCell cellB4 = rown2.createCell(n);
-	    cellB4.setCellValue(d.getCurrentUrl());
-	    
-	    /*HSSFCell cellA2 = rown.createCell(1);
-	    cellA2.setCellValue(FVTime);
-	    HSSFCell cellA3 = rown.createCell(2);
-	    cellA3.setCellValue(RVTime);
-	    
-	    */
 	    FileOutputStream fileout = new FileOutputStream("performance.xls");
-	    workbookr.write(fileout);    
+	    wb.write(fileout);    
 	    fileout.flush();
 	    fileout.close();
-	    filein.close();	
+	    fin.close();	
 	    d.close();
-		}catch(Exception E){System.out.println("File na mili");}
+		}catch(IOException E){System.out.println("File na mili");}
 	}
 }
 	
--- src/Progs/buffer.java
+++ src/Progs/buffer.java
@@ -1,31 +1,8 @@
 package Progs;
 
-import java.util.List;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.Test;
 
 
 public class buffer 
-{
-	@Test
-	public void foo()
-	{
-		System.out.println("inside foo function experiment branch");
-		WebDriver d = new FirefoxDriver();
-		d.get("http://www.webpagetest.org/result/150316_63_CYS/");
-		List<WebElement> ele= d.findElements(By.xpath("//table[@id='tableResults']/tbody/tr/td"));
-		System.out.println(ele.size());
-		for (WebElement e : ele)
-		{
-			System.out.println(e.getText());
-		}
-	}
-	public void bar()
-	{
-		System.out.println("inside bar for master");
-	}
+{	
+	
 }

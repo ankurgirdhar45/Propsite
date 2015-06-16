0862897de6334729160c0953ad2a1e6b87a4696d
ankur <ankur.girdhar@proptiger.com>
2015-06-16 15:43:14 +0530

    google, facebook sign in added

--- src/Progs/Login.java
+++ src/Progs/Login.java
@@ -4,11 +4,150 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class Login 
 {
+	WebDriverWait wait;
+	WebDriver driver;
+	
+	@BeforeClass
+		public void init()
+		{
+			System.setProperty("webdriver.chrome.driver","chromedriver");
+			driver = new ChromeDriver();
+			wait = new WebDriverWait(driver,10);
+		}
+
+		//@Test
+		public void gSignIn()
+		{
+			driver.manage().deleteAllCookies();
+			driver.get("https://www.proptiger.com");
+			//wait for sign in text to appear
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div[class='topMenuLinkBtn']")));
+			driver.findElement(By.cssSelector("div[class='topMenuLinkBtn']")).click();
+			
+			
+			//wait for Google sign in button to appear
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button[class='btn-google']")));
+			driver.findElement(By.cssSelector("button[class='btn-google']")).click();
+			try{
+				Thread.sleep(3000);}catch(InterruptedException e){System.out.println("Thread exception");}
+			
+			
+			/*Switch to new window*/ 
+			driver.switchTo().activeElement();	//
+			//driver.switchTo().window("Sign in - Google Accounts");
+			driver.switchTo().window(driver.getWindowHandles().toArray()[1].toString());
+			System.out.println(driver.getTitle());
+			
+			
+			//send value for email field in google window
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("Email")));
+			driver.findElement(By.id("Email")).sendKeys("dummyproptiger@gmail.com");
+			driver.findElement(By.id("next")).click();
+			
+			//send value for password field in google window
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[id='Passwd']")));
+			driver.findElement(By.id("Passwd")).sendKeys("dummyuse");
+			driver.findElement(By.id("signIn")).click();
+			
+			
+			try{Thread.sleep(3000);}catch(InterruptedException e){System.out.println("Thread exception");}	
+			driver.switchTo().window(driver.getWindowHandles().toArray()[0].toString());
+			validateLogin();
+		}
+		
+		
+		//@Test
+		public void fbSignIn()
+		{
+			driver.manage().deleteAllCookies();
+			System.out.println(driver.getWindowHandle().toString());
+			driver.navigate().refresh();
+			
+			//wait for sign in text to appear
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div[class='topMenuLinkBtn']")));
+			driver.findElement(By.cssSelector("div[class='topMenuLinkBtn']")).click();
+			
+			
+			//wait for Google sign in button to appear
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button[class='btn-facebook marginT10']")));
+			driver.findElement(By.cssSelector("button[class='btn-facebook marginT10']")).click();
+			try{Thread.sleep(3000);}catch(InterruptedException e){System.out.println("Thread exception");}
+			
+			
+			/*Switch to new window*/ 
+			driver.switchTo().window(driver.getWindowHandles().toArray()[1].toString());
+			System.out.println(driver.getTitle());
+			
+			
+			//send value for email field in google window
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("email")));
+			driver.findElement(By.id("email")).sendKeys("proptiger46@gmail.com");
+			driver.findElement(By.id("pass")).sendKeys("proptiger@46");
+			driver.findElement(By.id("u_0_2")).click();
+			
+			
+			try{Thread.sleep(3000);}catch(InterruptedException e){System.out.println("Thread exception");}	
+			driver.switchTo().window(driver.getWindowHandles().toArray()[0].toString());
+			validateLogin();
+		}
+	
+		
+	@Test	
+	public void signup()
+	{
+		driver.manage().deleteAllCookies();
+		driver.get("https://beta.proptiger-ws.com");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div[class='topMenuLinkBtn']")));
+		driver.findElement(By.cssSelector("div[class='topMenuLinkBtn']")).click();
+	
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[text()='Signup']")));
+		driver.findElement(By.xpath("//button[text()='Signup']")).click();
+		try{Thread.sleep(3000);}catch(InterruptedException e){System.out.println("Thread exception");}
+		
+		
+		//Fill the form for register
+		driver.findElement(By.xpath("//input[@data-ng-model='uname']")).sendKeys("qa-auto");
+		driver.findElement(By.xpath("//input[@data-ng-model='eid']")).sendKeys("qa@qa.qa");
+		driver.findElement(By.xpath("//input[@data-ng-model='pwd']")).sendKeys("123456");
+		
+		Select country  = new Select(driver.findElement(By.xpath("//select[@data-ng-model='country']")));
+		driver.findElement(By.xpath("//select[@data-ng-model='country']")).click();
+		country.selectByValue("0");
+		driver.findElement(By.xpath("//input[@data-ng-model='phn']")).sendKeys("1326412965");
+		driver.findElement(By.xpath("//button[text()='Signup']")).click();
+		
+	}
+	
+	public void invalidUserSignIn()
+	{
+		
+	}
+		
+		
+	public void validateLogin()
+	{
+		Assert.assertTrue(driver.findElement(By.xpath("//div[@class='dropdown-toggle topMenuLinkBtn ng-binding']")).getText()=="DUMMY");
+	}
+	//@AfterClass
+	public void closeWindow()
+	{
+		driver.close();
+		driver.quit();
+	}
+
+	
+	
 	@Test
 	public void login()
 	{
@@ -28,4 +167,6 @@ public class Login
 			System.out.println("User logged in by normal username and password, test case paxssed");
 		}
 	}
+	
+	
 }
--- src/Progs/buffer.java
+++ src/Progs/buffer.java
@@ -1,22 +1,18 @@
 package Progs;
 
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 //comment
@@ -41,8 +37,67 @@ public class buffer
 			System.out.println(e.getAttribute("href") + "text is " + e.getText());
 		}
 	}*/
+	//@Test
+	public void buffer1()
+	{
+		//d=new FirefoxDriver();
+		//d.get("https://www.proptiger.com");
+		//d.findElement(By.xpath("//body")).sendKeys(Keys.CONTROL + "t");
+		System.setProperty("webdriver.chrome.driver", "chromedriver");
+		d=new ChromeDriver();
+		d.get("http://www.proptiger.com");
+		try{
+		Thread.sleep(5000);
+		}catch(InterruptedException E){System.out.println("kuch bhi");}
+		//get title
+		String title = d.getTitle();
+		System.out.println(title);
+		WebElement e = d.findElement(By.cssSelector("meta[name='description']"));
+		System.out.println(e.getAttribute("content"));
+		e = d.findElement(By.cssSelector("meta[name='keywords']"));
+		System.out.println(e.getAttribute("content"));
+		System.out.println(d.findElement(By.tagName("H1")).getText());
+		System.out.println(d.findElement(By.tagName("H2")).getText());
+		
+		List<WebElement> images= d.findElements(By.tagName("img"));
+		for (WebElement f : images)
+		{
+			String alttext = (f.getAttribute("alt"));
+			if (alttext.isEmpty())
+			System.err.println("No alt text found for image " + f.getAttribute("src"));
+			else
+			System.out.println(alttext);
+		}
+		
+		
+		
+		
+		
+		JavascriptExecutor jse = (JavascriptExecutor)d;
+		jse.executeScript("window.scrollTo(0,document.body.scrollHeight)");
+		try{
+			Thread.sleep(2000);
+			}catch(InterruptedException E){System.out.println("kuch bhi");}
+		jse.executeScript("window.scrollTo(0,document.body.scrollHeight)");
+			
+		try{
+			Thread.sleep(2000);
+			}catch(InterruptedException E){System.out.println("kuch bhi");}
+		
+		jse.executeScript("window.scrollTo(0,0)");
+		try{
+			Thread.sleep(2000);
+			}catch(InterruptedException E){System.out.println("kuch bhi");}
+	
+	d.close();
+	}	
 	
 	
 	
-	
+	public void buffer2()
+	{
+		d=new FirefoxDriver();
+		d.get("https://www.proptiger.com/pune/kumar-urban-kruti-wadgaon-sheri-5000162/3bhk-3t-1368-sqft-apartment");
+		d.findElement(By.xpath("//"));
+	}
 }
